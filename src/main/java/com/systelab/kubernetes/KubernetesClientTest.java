815feffe78c2953419c001c12bc852fee4130c3e
alfonsserra <alfonsoserra@gmail.com>
2018-10-07 12:01:26 +0200

    Print Pod details

--- src/main/java/com/systelab/kubernetes/KubernetesClientTest.java
+++ src/main/java/com/systelab/kubernetes/KubernetesClientTest.java
@@ -1,8 +1,12 @@
 package com.systelab.kubernetes;
 
+import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.client.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class KubernetesClientTest {
 
@@ -15,24 +19,42 @@ public class KubernetesClientTest {
         client = new DefaultKubernetesClient(config);
     }
 
-    public void listPods() {
+    public void printPods() {
         try {
-            client.pods().list().getItems().stream().map(pod -> pod.getMetadata().getName()).forEach(System.out::println);
+            getPods("default").forEach((p) -> printPod(p));
 
         } catch (KubernetesClientException e) {
             logger.error(e.getMessage(), e);
         }
     }
 
+    private void printPod(Pod pod) {
+        System.out.println("Pod Details");
+        System.out.println("-------------------------------------");
+
+        System.out.println("Name:\t\t\t" + pod.getMetadata().getName());
+        System.out.println("Namespace:\t\t" + pod.getMetadata().getNamespace());
+        System.out.println("Labels:\t\t\t" + pod.getMetadata().getLabels().values().stream().collect(Collectors.joining(", ")));
+
+        System.out.println("Creation Time:\t" + pod.getMetadata().getCreationTimestamp());
+        System.out.println("Status\t\t\t" + pod.getStatus().getPhase());
+        System.out.println("QoS Class\t\t" + pod.getStatus().getQosClass());
+        System.out.println("");
+
+    }
+
+    private List<Pod> getPods(String nameSpace) {
+        return client.pods().inNamespace(nameSpace).list().getItems();
+    }
+
+
     public void close() {
         client.close();
     }
 
     public static void main(String... args) {
-
         KubernetesClientTest test = new KubernetesClientTest();
-        test.listPods();
+        test.printPods();
         test.close();
-
     }
 }
