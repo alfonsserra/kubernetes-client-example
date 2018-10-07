4c3710fa7c573db3995003a69b3dfce333a38584
alfonsserra <alfonsoserra@gmail.com>
2018-10-07 12:23:30 +0200

    Print Container details

--- src/main/java/com/systelab/kubernetes/KubernetesClientTest.java
+++ src/main/java/com/systelab/kubernetes/KubernetesClientTest.java
@@ -1,5 +1,6 @@
 package com.systelab.kubernetes;
 
+import io.fabric8.kubernetes.api.model.Container;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.client.*;
 import org.slf4j.Logger;
@@ -37,11 +38,32 @@ public class KubernetesClientTest {
         System.out.println("Labels:\t\t\t" + pod.getMetadata().getLabels().values().stream().collect(Collectors.joining(", ")));
 
         System.out.println("Creation Time:\t" + pod.getMetadata().getCreationTimestamp());
-        System.out.println("Status\t\t\t" + pod.getStatus().getPhase());
-        System.out.println("QoS Class\t\t" + pod.getStatus().getQosClass());
+        System.out.println("Status:\t\t\t" + pod.getStatus().getPhase());
+        System.out.println("QoS Class:\t\t" + pod.getStatus().getQosClass());
+
+        printContainers(pod.getSpec().getContainers());
         System.out.println("");
 
     }
+
+    private void printContainers(List<Container> containers) {
+        System.out.println("Containers:");
+        containers.forEach(c -> printContainer(c));
+    }
+
+    private void printContainer(Container container) {
+        System.out.println("\t" + container.getName());
+        System.out.println("\t\tImage:\t\t" + container.getImage());
+        if (container.getEnv().size() > 0) {
+            System.out.println("\t\tEnvironment:");
+            container.getEnv().forEach((e) -> System.out.println("\t\t\t\t\t" + e.getName() + ": " + e.getValue()));
+        }
+        if (container.getPorts().size() > 0) {
+            System.out.println("\t\tPorts:");
+            container.getPorts().forEach((p) -> System.out.println("\t\t\t\t\t" + p.getContainerPort()+":"+p.getHostPort()));
+        }
+    }
+
 
     private List<Pod> getPods(String nameSpace) {
         return client.pods().inNamespace(nameSpace).list().getItems();
