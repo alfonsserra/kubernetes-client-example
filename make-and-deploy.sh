5a713272c111b939a37ee1f79d975fbddef27093
alfonsserra <alfonsoserra@gmail.com>
2018-10-08 14:57:43 +0200

    delete fabric8-rbac.yaml before applying it

--- make-and-deploy.sh
+++ make-and-deploy.sh
@@ -1,6 +1,7 @@
 mvn clean package
 docker build -t systelab/kubernetes-client .
 docker push systelab/kubernetes-client
+kubectl delete -f fabric8-rbac.yaml
 kubectl apply -f fabric8-rbac.yaml
 kubectl delete deploy kubernetes-client-deploy
 kubectl create -f kubernetes-client-deploy.yml
